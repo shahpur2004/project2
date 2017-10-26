d783fe7248c8277fa4db1cbf7951c29581d91956
shahpur <skarimi3@horizon.csueastbay.edu>
2017-10-26 10:38:33 -0700

    new commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'my project2 push!' });
+  res.render('index', { title: 'my project2 push number 2!' });
 });
 
 module.exports = router;
